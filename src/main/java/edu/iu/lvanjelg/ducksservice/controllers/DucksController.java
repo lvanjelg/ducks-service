9a20920fe493c3677da680fc916849924ccf54c5
lvanjelg <lukevanjelg@gmail.com>
2024-02-07 15:30:05 -0500

    .

--- src/main/java/edu/iu/lvanjelg/ducksservice/controllers/DucksController.java
+++ src/main/java/edu/iu/lvanjelg/ducksservice/controllers/DucksController.java
@@ -1,11 +1,38 @@
 package edu.iu.lvanjelg.ducksservice.controllers;
 
-import edu.iu.lvanjelg.ducksservice.models.Duck;
+import edu.iu.lvanjelg.ducksservice.repository.DucksRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.IOException;
 
+@RestController
+@RequestMapping("/ducks")
 public class DucksController {
-        public static void blank(){
-
+    private DucksRepository ducksRepository;
+    public DucksController(DucksRepository ducksRepository){
+        this.ducksRepository = ducksRepository;
     }
+    @PostMapping("/ducks/{id}/image")
+    public boolean uploadImage(@PathVariable int id, @RequestParam MultipartFile file){
+        try {
+            return ducksRepository.uploadImage(id,file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @GetMapping("/ducks/{id}/image")
+    public ResponseEntity<?> getImage(@PathVariable int id){
+        byte[] image = new byte[0];
+        try {
+            image = ducksRepository.downloadImage(id);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.status(HttpStatus.FOUND).contentType(MediaType.IMAGE_PNG).body(image);
+    }
+
 }
--- src/main/java/edu/iu/lvanjelg/ducksservice/repository/DucksRepository.java
+++ src/main/java/edu/iu/lvanjelg/ducksservice/repository/DucksRepository.java
@@ -1,11 +1,18 @@
 package edu.iu.lvanjelg.ducksservice.repository;
 
 import edu.iu.lvanjelg.ducksservice.models.Duck;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.lang.reflect.Array;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class DucksRepository {
+    private static final String IMAGES_FOLDER_PATH = "ducks/images";
     public static ArrayList<Duck> ducks = new ArrayList<>();
     public static boolean addDuck(Duck d){
         ducks.add(d);
@@ -33,11 +40,17 @@ public class DucksRepository {
         }
         return dList;
     }
-    public static boolean uploadImage(){
+    public boolean uploadImage(int id, MultipartFile file) throws IOException {
+        String fileExtension = ".png";
+        Path path = Paths.get(IMAGES_FOLDER_PATH + id + fileExtension);
+        file.transferTo(path);
         return true;
     }
-    public static String downloadImage(){
-        return "";
+    public static byte[] downloadImage(int id) throws IOException {
+        String fileExtension = ".png";
+        Path path = Paths.get(IMAGES_FOLDER_PATH + id + fileExtension);
+        byte[] image = Files.readAllBytes(path);
+        return image;
     }
     public static boolean uploadAudio(){
         return true;
